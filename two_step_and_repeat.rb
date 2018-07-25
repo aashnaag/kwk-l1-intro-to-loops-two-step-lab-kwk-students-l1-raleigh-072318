d6d00baad5bace172c0a859ba044b829c2b3781b
aashnaag <aashna.agarwal55@gmail.com>
2018-07-25 14:24:07 +0000

    automatically backed up by learn

--- two_step_and_repeat.rb
+++ two_step_and_repeat.rb
@@ -34,8 +34,17 @@ end
 
 def how_many_steps?
   steps = 0 
-  
+loop do
+  steps += 1 
+  puts steps
+if steps % 2 == 0 
+  puts "Left"
+else 
+  puts "Right"
 end
+sleep(0.5)
+end 
+end 
 
 def break_dance
   # Write a solution that uses the same code as how_many_steps?, but breaks the
